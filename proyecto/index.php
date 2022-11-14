0294e52784d3519a61d6d3e410da930898d1a2fa
mayenkaf <mayenkaf@gmail.com>
2022-11-14 01:36:20 -0500

    Create index.php

---
+++ proyecto/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Proyecto 44</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+
+<div class="sidenav">
+  <a href="#">Home</a>
+  <?php 
+    echo "<a href='merchandise.php?t=Tshirts&&id=0'>Merchandise</a>";
+  ?>
+  <a href="management.php">Management</a>
+</div>
+
+<div class="content">
+  <h2>Team Lewis Hamilton</h2>
+  <div>
+    <img src="src/lewis-hamilton.jpg"></img>
+    <p >
+        As founder of X44, I am looking forward to building my team around important values such as sustainability and equality. 
+        None of us are perfect and we all have improvements to make, 
+        but I am excited to use our platform to highlight the most serious 
+        issues facing our planet and the solutions we can all be part of.<br><br>
+        <span>Lewis Hamilton</span>
+    </p>
+  </div>
+  </div>
+</body>
+</html>
